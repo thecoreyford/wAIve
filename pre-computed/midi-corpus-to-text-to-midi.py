84ded918187e0b7d9a57561c1d8f2bc8554fd735
Corey Ford <c.j.ford@qmul.ac.uk>
2022-05-25 18:38:16 +0100

    MOD: Dataset dosen't need velocity, and note ranges are now more restricted.

--- pre-computed/midi-corpus-to-text-to-midi.py
+++ pre-computed/midi-corpus-to-text-to-midi.py
@@ -3,9 +3,10 @@ import music21 as m21
 
 def midi_corpus_to_text():
 	# Load and tokenize the data 
-	filenames = glob.glob("data/new-wave/*.mid")
+	filenames = glob.glob("dataset/new-wave/*.mid")
 
 	music = []
+
 	for files in range (len(filenames)):	
 		print(filenames[files])
 		
@@ -14,18 +15,14 @@ def midi_corpus_to_text():
 		
 		for parts in midi:
 			for n in parts: #<--- for the different parts
-				if "inst" in str(n):
-					print("instruments")
-
 				if "rest" not in str(n):
 					if "note" in str(n):
 						# Only add notes that can be used in our interface... 
 						if n.pitch.midi in [72, 71, 69, 67, 65, 64, 62, 60]:
 							# and not too long like finishing notes 
-							if n.duration.quarterLength <= 8.0:
+							if n.duration.quarterLength <= 2.0:
 								# Append text representation 
-								music.append("n:" + str(n.pitch.nameWithOctave)
-							       + "v:80" + "d:" +
+								music.append("n:" + str(n.pitch.nameWithOctave) + "d:" +
 							       str(n.duration.quarterLength));
 
 	# Write to file
@@ -50,7 +47,7 @@ def text_to_midi_corpus(my_file):
 			midi_pitch = line.strip()[2:5]
 		note = m21.note.Note(midi_pitch)
 		
-		duration = line.strip()[8:].replace("d:","")
+		duration = line.strip()[6:].replace("d:","")
 		if "/" in duration:
 			x = duration.split("/")
 			duration = float(x[0]) / float(x[1])
@@ -72,5 +69,5 @@ def text_to_midi_corpus(my_file):
 #============================================================================
 
 if __name__ == "__main__":
-	midi_corpus_to_text ()
-	text_to_midi_corpus ("new-wave.txt")
+	midi_corpus_to_text()
+	text_to_midi_corpus("new-wave.txt")
