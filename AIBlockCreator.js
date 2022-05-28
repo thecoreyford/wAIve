9bf2e1ec68fe5e6ba8b4b39534a81de8eb8fe086
Corey Ford <c.j.ford@qmul.ac.uk>
2022-05-28 12:36:38 +0100

    MOD: Added whitespace

--- AIBlockCreator.js
+++ AIBlockCreator.js
@@ -22,25 +22,25 @@ class AIBlockCreator
 
 
 		// TODO: if 25 seconds do the stuff in here
-		if (keyIsDown(LEFT_ARROW) && this.onetime === true) 
+		if (keyIsDown (LEFT_ARROW) && this.onetime === true) 
 		{
 			// For the top left 
 			musicBlocks.push (new AIBlock (anchor1.x, anchor1.y, 200, 100));
-			musicBlocks.push (new AIBlock (anchor1.x + random(230,140), anchor1.y + random(-10, 30), 200, 100));
-			musicBlocks.push (new AIBlock (anchor1.x + random(-10,20), anchor1.y + random(80,110), 200, 100));
-			musicBlocks.push (new AIBlock (anchor1.x + random(-10,20), anchor1.y + random(180,210), 200, 100));
+			musicBlocks.push (new AIBlock (anchor1.x + random (230, 140), anchor1.y + random (-10, 30), 200, 100));
+			musicBlocks.push (new AIBlock (anchor1.x + random (-10, 20), anchor1.y + random (80, 110), 200, 100));
+			musicBlocks.push (new AIBlock (anchor1.x + random (-10, 20), anchor1.y + random (180, 210), 200, 100));
 
 			// For the top right
 			musicBlocks.push (new AIBlock (anchor2.x - 300, anchor2.y - 30, 200, 100));
-			musicBlocks.push (new AIBlock (anchor2.x - random(100,200), anchor2.y + random(-10, 30), 200, 100));
-			musicBlocks.push (new AIBlock (anchor2.x + random(0,20), anchor2.y + random(80,110), 200, 100));
-			musicBlocks.push (new AIBlock (anchor2.x - random(30,50), anchor2.y - 40, 200, 100));
+			musicBlocks.push (new AIBlock (anchor2.x - random (100, 200), anchor2.y + random (-10, 30), 200, 100));
+			musicBlocks.push (new AIBlock (anchor2.x + random (0, 20), anchor2.y + random (80, 110), 200, 100));
+			musicBlocks.push (new AIBlock (anchor2.x - random (30, 50), anchor2.y - 40, 200, 100));
 
 			// For the bottom left 
 			musicBlocks.push (new AIBlock (anchor3.x + 300, anchor3.y - 30, 200, 100));
-			musicBlocks.push (new AIBlock (anchor3.x + random(100,200), anchor3.y - random(-10, 30), 200, 100));
-			musicBlocks.push (new AIBlock (anchor3.x + random(0,20) - 25, anchor3.y - random(80,110), 200, 100));
-			musicBlocks.push (new AIBlock (anchor3.x + random(30,40), anchor3.y - 40, 200, 100));
+			musicBlocks.push (new AIBlock (anchor3.x + random (100, 200), anchor3.y - random (-10, 30), 200, 100));
+			musicBlocks.push (new AIBlock (anchor3.x + random (0, 20) - 25, anchor3.y - random (80, 110), 200, 100));
+			musicBlocks.push (new AIBlock (anchor3.x + random (30, 40), anchor3.y - 40, 200, 100));
 
    			this.onetime=false;
   		}
