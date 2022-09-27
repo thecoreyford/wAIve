8f8c3e1fc336ea7ff43cf3a126ba4162366a9235
Corey Ford <c.j.ford@qmul.ac.uk>
2022-09-27 13:07:06 +0100

    ADD: Highlighting for dissonance, descending phrases and thirds.

--- colours.js
+++ colours.js
@@ -9,10 +9,12 @@ const darkGrey = "#757575";
 
 const orange = "#F0A034";
 
+const purple = "#AA00D7";
+
 const green = "#40b44c";
 
 const googGreen = "#00e26b";
 
 const red = "#de5246";
 
-const yellow = "#F4B400";
+const yellow = "#FBBC05";
