d799bd8d4f5fedd3ffcb2dcbab9be2d48ac9c2e7
Corey Ford <c.j.ford@qmul.ac.uk>
2022-10-07 11:26:36 +0100

    MOD: Tweaked AI to cluster blocks by musical metric, but with and example of each of the three colours.

--- AIBlockCreator.js
+++ AIBlockCreator.js
@@ -42,14 +42,14 @@ class AIBlockCreator
 
 
 		// // For the top left (PITCH COUNT)
-		var values = musicMetrics.getMostSimiliarValues("pitchCount", generated_data, 40);
-		values = musicMetrics.getMostSimiliarValues("pitchRange", values, 30);	
-		values = musicMetrics.getMostSimiliarValues("averagePitchInterval", values, 20);					
-		values = musicMetrics.getMostColourValues(orange, values, 10);
+		var valuesMusic = musicMetrics.getMostSimiliarValues("pitchCount", generated_data, 15);
+		var values = musicMetrics.getMostColourValues(orange, valuesMusic, 8);
 		musicBlocks.push (new AIBlock (anchor1.x, anchor1.y + random(0,5), 200, 100,
 									   values[Math.floor(Math.random()*values.length)]["music_grid"]));
+		values = musicMetrics.getMostColourValues(googGreen, valuesMusic, 8);
 		musicBlocks.push (new AIBlock (anchor1.x + random (230, 220), anchor1.y, 200, 100,
 									   values[Math.floor(Math.random()*values.length)]["music_grid"]));
+		values = musicMetrics.getMostColourValues(purple, valuesMusic, 8);
 		musicBlocks.push (new AIBlock (anchor1.x, anchor1.y + random (100, 120), 200, 100,
 									   values[Math.floor(Math.random()*values.length)]["music_grid"]));
 
@@ -64,27 +64,29 @@ class AIBlockCreator
 
 		// //// For the top right (PITCH RANGE)
 		// // var values = musicMetrics.getMostSimiliarValues("pitchRange");	
-		var values = musicMetrics.getMostSimiliarValues("pitchCount", generated_data, 40);
-		values = musicMetrics.getMostSimiliarValues("pitchRange", values, 30);	
-		values = musicMetrics.getMostSimiliarValues("averagePitchInterval", values, 20);					
-		values = musicMetrics.getMostColourValues(googGreen, values, 10);				
+		valuesMusic = musicMetrics.getMostSimiliarValues("pitchRange", generated_data, 15);	
+		values = musicMetrics.getMostColourValues(googGreen, valuesMusic, 8);				
 		musicBlocks.push (new AIBlock (anchor2.x - 300, anchor2.y - random(40, 30), 200, 100,
 									    values[Math.floor(Math.random()*values.length)]["music_grid"]));
+		values = musicMetrics.getMostColourValues(orange, valuesMusic, 8);				
 		musicBlocks.push (new AIBlock (anchor2.x - 80, anchor2.y - random (25, 30), 200, 100,
 									    values[Math.floor(Math.random()*values.length)]["music_grid"]));
+		values = musicMetrics.getMostColourValues(purple, valuesMusic, 8);				
 		musicBlocks.push (new AIBlock (anchor2.x - 25, anchor2.y + 75, 200, 100,
 									    values[Math.floor(Math.random()*values.length)]["music_grid"]));
 
 		// // // For the bottom left (AVERAGE PITCH INTERVAL)
 		// var values = musicMetrics.getMostSimiliarValues("averagePitchInterval");					
-		var values = musicMetrics.getMostSimiliarValues("pitchCount", generated_data, 40);
-		values = musicMetrics.getMostSimiliarValues("pitchRange", values, 30);	
-		values = musicMetrics.getMostSimiliarValues("averagePitchInterval", values, 20);					
-		values = musicMetrics.getMostColourValues(purple, values, 10);				
+		// var values = musicMetrics.getMostSimiliarValues("pitchCount", generated_data, 40);
+		// values = musicMetrics.getMostSimiliarValues("pitchRange", values, 30);	
+		valuesMusic = musicMetrics.getMostSimiliarValues("averagePitchInterval", generated_data, 15);					
+		values = musicMetrics.getMostColourValues(purple, valuesMusic, 8);				
 		musicBlocks.push (new AIBlock (anchor3.x - 30, anchor3.y - 350, 200, 100,
 										values[Math.floor(Math.random()*values.length)]["music_grid"]));
+		values = musicMetrics.getMostColourValues(orange, valuesMusic, 8);				
 		musicBlocks.push (new AIBlock (anchor3.x - 33, anchor3.y - random (250, 200), 200, 100,
 										values[Math.floor(Math.random()*values.length)]["music_grid"]));
+		values = musicMetrics.getMostColourValues(googGreen, valuesMusic, 8);				
 		musicBlocks.push (new AIBlock (anchor3.x - random(-30,-15) - 40, anchor3.y - random(80,100), 200, 100,
 										values[Math.floor(Math.random()*values.length)]["music_grid"]));
 	}
