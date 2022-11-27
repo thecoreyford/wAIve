1d53e701f2acfe1af141b8a900a53ecc447efaf8
Corey Ford <c.j.ford@qmul.ac.uk>
2022-11-27 14:02:09 +0000

    MOD: Selected some different colours

--- colours.js
+++ colours.js
@@ -3,23 +3,23 @@
 
 //================================
 
-const djLightGrey = "#14213d";//#315a80";//"#2e2e2e";
+const djLightGrey = "#1f2c65"; //"#14213d";//#315a80";//"#2e2e2e";
 
-const djBlack = "#315a80"//"#131313";
+const djBlack = "#5052b3";//"#315a80"//"#131313";
 
-const djOrange = "#ce5e32";
+const djOrange = "#4de0f8"; //"#4EDFFC";//"#ce5e32";
 
-const djLightLightGrey = "#828282";
+const djLightLightGrey = "#A6A6A8";//"#cdbef5";//"#828282";
 
 const djOrange1 = "#f5ded6";
 const djOrange2 = "#ebbead";
 const djOrange3 = "#d77e5a";
 const djOrange4 = "#ce5e32";
 
-const djPink = "#ef5eba";
+const djPink = "#F4A4FD";//"#ef5eba";
 
 const djGreen = "#1dc75e";
-const djGreen2 = "#88c93d"; // the one i'm using 
+const djGreen2 = "#FF785A"//"#88c93d"; // the one i'm using 
 
 
 //====================================
@@ -30,7 +30,7 @@ const veryDarkBlue = "#1750AC";
 
 const lightBlue = "#C4DBFA";
 
-const lightGrey = "#d9d9d9";//"#F7F7F7";
+const lightGrey = "#F7F5FB";//"#d9d9d9";//"#F7F7F7";
 
 const darkGrey = "#757575";
 
@@ -44,8 +44,8 @@ const green = "#40b44c";
 
 const googGreen = "#00e26b";
 
-const red = "#ae0600";//"#6f0000";
+const red = "#DB2B39"; //"#ae0600";//"#6f0000";
 
-const yellow = "#FBBC05";
+const yellow = "#FFCB47"; //"#FBBC05";
 
 const white = "#FFFFFF";
